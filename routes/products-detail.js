86cf637de281cf571cae551915c78c959526add9
DarioVarto <dariovartolo@hotmail.com>
2023-07-11 23:33:40 -0300

    "Carrito funcionando"

--- routes/products-detail.js
+++ routes/products-detail.js
@@ -1,8 +1,9 @@
 import express from 'express'
 const router=express.Router()
 import passport from 'passport'
-
+import mongoose from 'mongoose'
 import Producto from '../models/products.js'
+
 
 // Crear una función para cargar los productos en la base de datos
  async function cargarProductos() {
@@ -50,31 +51,122 @@ router.get('/prod', (req, res) => {
     })
 
 })
+
+
 router.post('/carrito/agregar', (req, res) => {
-  const { nombre, descripcion, precio,stock } = req.body;
+  const { nombre, descripcion, precio, stock } = req.body;
   
   if (!req.session.carrito) {
     req.session.carrito = [];
-    req.session.total = 0; // Inicializa el total en 0 si el carrito no existe
+    req.session.total = 0; // Establece el total en 0 si el carrito no existe
   }
   
-  const precioNum = parseFloat(precio); // Convierte el precio a un número
-  const stockNum = parseInt(stock)
-  req.session.carrito.push({ nombre, descripcion, precio: precioNum, stock:stockNum }); // Guarda el precio como un número en el carrito
+  const precioNum = parseFloat(precio);
+  const stockNum = parseInt(stock);
+  const producto = {
+    nombre,
+    descripcion,
+    precio: precioNum,
+    stock: stockNum,
+    cantidad: 1
+  };
   
-  req.session.total += precioNum; // Actualiza la suma total
+  req.session.carrito.push(producto);
+  req.session.total += producto.precio; // Suma solo el precio del producto agregado
   
   res.redirect('/carrito');
 });
+
+router.post('/carrito/sumar/:index', (req, res) => {
+  const index = req.params.index;
+  
+  if (req.session.carrito && req.session.carrito.length > index) {
+    const producto = req.session.carrito[index];
+    
+    if (producto.stock > 0) {
+      if (!producto.cantidad) {
+        producto.cantidad = 1;
+      }
+      producto.cantidad += 1;
+      producto.stock -= 1;
+      req.session.total += producto.precio;
+    }
+  }
+  
+  res.redirect('/carrito');
+});
+
+router.post('/carrito/restar/:index', (req, res) => {
+  const index = req.params.index;
+  
+  if (req.session.carrito && req.session.carrito.length > index) {
+    const producto = req.session.carrito[index];
+    
+    if (producto.cantidad > 1) {
+      producto.cantidad -= 1;
+      producto.stock += 1;
+      req.session.total -= producto.precio; // Resta solo el precio del producto restado
+    } else {
+      req.session.carrito.splice(index, 1);
+    }
+  }
+  
+  res.redirect('/carrito');
+});
+
 router.get('/carrito', (req, res) => {
   const carrito = req.session.carrito || [];
   const total = req.session.total || 0; // Obtiene el total de la sesión o establece 0 si no existe
   
   res.render('products/carrito', { carrito, total }); // Pasa el total a la vista
 });
+router.post('/carrito/eliminar/:index', (req, res) => {
+  const index = req.params.index;
+  
+  if (req.session.carrito && req.session.carrito.length > index) {
+    const producto = req.session.carrito[index];
+    req.session.total -= producto.precio * (producto.cantidad || 1);
+    req.session.carrito.splice(index, 1);
+  }
+  
+  res.redirect('/carrito');
+});
 
+router.get('/compraRealizada', async (req, res) => {
+  const carrito = req.session.carrito || [];
 
+  try {
+    const productosCollection = mongoose.connection.collection('productos');
 
+    // Actualizar el stock de cada producto en la colección
+    for (const producto of carrito) {
+      const { nombre, cantidad } = producto;
+
+      await productosCollection.updateOne(
+        { nombre: nombre },
+        { $inc: { stock: -cantidad } } // Restar la cantidad del producto al stock
+      );
+    }
+
+    // Vaciar el carrito y redirigir a la página principal o a otra página de éxito
+    req.session.carrito = [];
+    req.session.total = 0;
+    res.redirect('/');
+  } catch (error) {
+    console.error('Error al actualizar el stock en la base de datos:', error);
+    res.redirect('/carrito'); // Redirigir a la página del carrito en caso de error
+  }
+});
+
+// Disminuye el stock en la base de datos
+ /*  try {
+    await Producto.findOneAndUpdate(
+      { nombre: nombre },
+      { $inc: { stock: -1 } } // Resta 1 al stock
+    );
+  } catch (error) {
+    console.log('Error al disminuir el stock en la base de datos:', error);
+  } */
 export default router
 
 
